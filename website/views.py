f38bc75919095e07c6f5a27b634438f9a9f191e0
Basith P <basithpofficial@gmail.com>
2023-08-29 22:35:40 +0530

    feat: login

--- website/views.py
+++ website/views.py
@@ -1,6 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 
 def home(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You have been logged in!")
+        else:
+            messages.success(request, "Error!")
+        return redirect('home')
+
     context = {}
     return render(request, 'home.html', context)
+
+
+def login_view(request):
+    pass
+
+
+def logout_view(request):
+    pass
