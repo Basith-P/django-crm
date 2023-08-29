19ee107d7ff2575c506f055351098809e5d89d82
Basith P <basithpofficial@gmail.com>
2023-08-29 22:44:47 +0530

    feat: logout

--- website/views.py
+++ website/views.py
@@ -12,16 +12,14 @@ def home(request):
             login(request, user)
             messages.success(request, "You have been logged in!")
         else:
-            messages.success(request, "Error!")
+            messages.warning(request, "Error!")
         return redirect('home')
 
     context = {}
     return render(request, 'home.html', context)
 
 
-def login_view(request):
-    pass
-
-
 def logout_view(request):
-    pass
+    logout(request)
+    messages.success(request, "You have been logged out!")
+    return redirect('home')
